60eca2a3db35fb2a6b492a90b208f2198b282ba9
mikel <mikelg_15@hotmail.com>
2020-05-16 18:20:45 +0200

    Texto
    A Castellano

--- atenea/src/Migrations/Version20200516160137.php
+++ atenea/src/Migrations/Version20200516160137.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200507155949 extends AbstractMigration
+final class Version20200516160137 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20200507155949 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE usuarios DROP admin_super_user');
+        $this->addSql('ALTER TABLE subtipos DROP interno');
+        $this->addSql('ALTER TABLE cuestiones DROP ineterno');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +31,7 @@ final class Version20200507155949 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE usuarios ADD admin_super_user INT NOT NULL');
+        $this->addSql('ALTER TABLE cuestiones ADD ineterno TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE subtipos ADD interno TINYINT(1) NOT NULL');
     }
 }
